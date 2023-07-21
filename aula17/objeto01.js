3144d0de7f189fa75b88f85a62f86809643155dc
Alberto Lima <131916622+albertobtlima@users.noreply.github.com>
2023-07-21 12:15:54 -0300

    update

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -1,2 +1,7 @@
-let amigo = {}
-console.log(typeof amigo)
+let amigo = {nome: 'Jose', sexo: 'M', peso: 85.4, engordar(p){
+    console.log('Engordou')
+    this.peso += p
+}}
+
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
